4c64d3d8944ac3b367ac221ed01742c3c534d52a
CylanTomacio <elvisdanielbriceno@gmail.com>
2023-01-30 11:28:41 -0500

    Finalizado el Hero.jsx, creado el About.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
+import { About } from "./components/About";
 
 export const App = () => {
 
@@ -7,6 +8,7 @@ export const App = () => {
     <>
       <Navbar />
       <Hero />
+      <About />
     </>
   )
 }
